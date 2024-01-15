1b0477c940a51e08fba109e91cb56a43c72459b6
Eric LaFontsee <elafontsee@gmail.com>
2024-01-15 06:48:44 -0600

    Worked on the mobile responsiveness for the hero image.

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -5,16 +5,16 @@ const Header = () => {
   return (
     <header className="container-fluid hero__container">
       <div className="container py-lg-3 py-xl-5 my-5 z-3">
-        <div className="row pt-md-3 py-xxl-5 my-5">
-          <div className="col-sm-9 col-md-8 col-lg-5 pb-3 pb-sm-0 mt-md-n5 mb-4 mb-sm-5">
-            <div className="ps-md-4 ps-lg-0 mt-md-n4 mt-lg-n5">
+        <div className="row my-5">
+          <div className="col-sm-9 col-md-8 col-lg-5 header-copy p-3">
+            <div className="">
               <h1 className="display-5 mb-lg-4">
-                Hi, I'm <span className="fw-bolder">Nicole Kruse</span>
+                Hi, I'm <span className="fw-bolder">Nicole</span>
               </h1>
               <p className="text-body fs-lg">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur posuere risus metus, sed venenatis.</p>
             </div>
             <div className="mt-5">
-              <button className='btn btn-primary'>Get Started Today!</button>
+              <button className='btn btn-primary'>Let's Chat!</button>
             </div>
           </div>
         </div>
