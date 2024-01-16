3592565fd7a4af6cc6868fac4af321c5d145fdf7
Eric LaFontsee <elafontsee@gmail.com>
2024-01-16 07:17:20 -0600

    commented out react router for now

--- src/App.js
+++ src/App.js
@@ -1,17 +1,18 @@
 import React from "react";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+// import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
 import Home from "./pages/home/Home";
-import PageNotFound from "./pages/pageNotFound/PageNotFound";
+// import PageNotFound from "./pages/pageNotFound/PageNotFound";
 
 
-const router = createBrowserRouter([
-  { path: "/", element: <Home /> },
-  { path: "/*", element: <PageNotFound /> }
-]);
+// const router = createBrowserRouter([
+//   { path: "/", element: <Home /> },
+//   { path: "/*", element: <PageNotFound /> }
+// ]);
 
 const App = () => {
-  return <RouterProvider router={router} />;
+  // return <RouterProvider router={router} />;
+  return <Home />;
 };
 
 export default App;
