6616ed674b48a5a6f349569623c78fc996c6eb94
Eric LaFontsee <elafontsee@gmail.com>
2024-02-04 09:23:41 -0600

    uncommented react router

--- src/App.js
+++ src/App.js
@@ -1,18 +1,17 @@
 import React from "react";
-// import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
 import Home from "./pages/home/Home";
-// import PageNotFound from "./pages/pageNotFound/PageNotFound";
+import PageNotFound from "./pages/pageNotFound/PageNotFound";
 
 
-// const router = createBrowserRouter([
-//   { path: "/", element: <Home /> },
-//   { path: "/*", element: <PageNotFound /> }
-// ]);
+const router = createBrowserRouter([
+  { path: "/", element: <Home /> },
+  { path: "/*", element: <PageNotFound /> }
+]);
 
 const App = () => {
-  // return <RouterProvider router={router} />;
-  return <Home />;
+  return <RouterProvider router={router} />;
 };
 
 export default App;
