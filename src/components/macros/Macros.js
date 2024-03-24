f1cd009d6a4598f174145e11b0c1db135d86773f
Eric LaFontsee <elafontsee@gmail.com>
2024-03-24 11:34:23 -0500

    added new portrait to macro counting section

--- src/components/macros/Macros.js
+++ src/components/macros/Macros.js
@@ -1,5 +1,5 @@
 import React from "react";
-import PHONE from "../../assets/phone.png";
+import MACROPORTRAIT from "../../assets/macro-coaching.jpg";
 import { CiViewList } from "react-icons/ci";
 import { BsFillPeopleFill } from "react-icons/bs";
 import { LiaBookReaderSolid } from "react-icons/lia";
@@ -10,9 +10,9 @@ const Macros = () => {
   return (
     <section id="macros">
       <h2 className="pb-3 mb-2 mb-lg-3 mb-xl-4">One Time Macro Counts</h2>
-      <div className="container coaching__container">
+      <div className="container macros__container">
         <div className="row align-items-center justify-content-evenly mt-5">
-          <div className="order-2 order-lg-1 col-10 col-lg-6 mt-5 mt-lg-0 coaching__list">
+          <div className="order-2 order-lg-1 col-10 col-lg-6 mt-5 mt-lg-0 macros__list">
             <div className="ps-lg-4 ps-xl-0">
               <h3 className="pb-3 mb-2 mb-lg-3 mb-xl-4">What’s included:</h3>
               <ul className="list-unstyled pb-1 pb-xl-2">
@@ -40,11 +40,11 @@ const Macros = () => {
               </ul>
             </div>
           </div>
-          <div className="order-1 order-lg-2 col-10 col-lg-4 about__portrait">
+          <div className="order-1 order-lg-2 col-10 col-lg-4 macros__portrait">
             <img
-              className="img-fluid mx-auto coaching__portrait--image"
-              src={PHONE}
-              alt="DALL-E generated image of a phone with a pie chart depicting Macro Nutrient Counting"
+              className="img-fluid mx-auto macros__portrait--image"
+              src={MACROPORTRAIT}
+              alt="Portrait of Nicole Kruse"
             />
           </div>
         </div>
